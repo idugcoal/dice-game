f05ba0c5f113d6acf2d3eb7e9b2565bbe8e70179
Doug Cole <idugcoal@gmail.com>
2020-01-12 12:02:53 -0800

    separate fabricator from factory

---
+++ src/components/fabricator.jsx
@@ -0,0 +1,32 @@
+export const getWorkcenters = ({
+  numWorkcenters,
+  wipPerWorkcenter,
+  wipPerConstraint,
+}) => {
+  /** Helper functions */
+  const isConstraintIndex = index => {
+    return index === Math.round(numWorkcenters * 0.67) - 1
+  }
+
+  const buildWorkcenters = () => {
+    const _workcenters = []
+    for (let i = 0; i < numWorkcenters; i++) {
+      const isConstraint = isConstraintIndex(i)
+      _workcenters.push({
+        key: i,
+        number: i + 1,
+        isConstraint: isConstraint,
+        wip: isConstraint ? wipPerConstraint : wipPerWorkcenter,
+      })
+    }
+    return _workcenters
+  }
+
+  /** Initial config */
+  const workcenters = buildWorkcenters()
+
+  /** Template */
+  return workcenters
+}
+
+// export default getWorkcenters
--- src/components/factory.jsx
+++ src/components/factory.jsx
@@ -1,30 +1,7 @@
 import React from 'react'
 import Workcenter from './workcenter'
 
-const Factory = ({ numWorkcenters, wipPerWorkcenter, wipPerConstraint }) => {
-  /** Helper functions */
-  const isConstraintIndex = index => {
-    return index === Math.round(numWorkcenters * 0.67) - 1
-  }
-
-  const buildWorkcenters = () => {
-    const _workcenters = []
-    for (let i = 0; i < numWorkcenters; i++) {
-      const isConstraint = isConstraintIndex(i)
-      _workcenters.push({
-        key: i,
-        number: i + 1,
-        isConstraint: isConstraint,
-        wip: isConstraint ? wipPerConstraint : wipPerWorkcenter,
-      })
-    }
-    console.log('workcenters', _workcenters)
-    return _workcenters
-  }
-
-  /** Initial config */
-  const workcenters = buildWorkcenters()
-
+const Factory = ({ workcenters }) => {
   /** Template */
   return (
     <div style={styles.container}>
