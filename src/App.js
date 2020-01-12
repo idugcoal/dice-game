3f82280d8dbc344a9d1cf795228a59e7ed2ec068
Doug Cole <idugcoal@gmail.com>
2020-01-11 16:30:29 -0800

    create initial state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import UserInputs from './components/user_inputs'
 import Factory from './components/factory'
 
 function App() {
+  const _numWorkcenters = 6
+  const _workcenters = []
+  for (let i = 0; i < _numWorkcenters; i++) {
+    _workcenters.push(i)
+  }
+
+  const [workcenters] = useState(_workcenters)
+  const [runNumber, setRunNumber] = useState()
+  const [numWorkdays, setNumWorkdays] = useState()
+  const [numWorkcenters, setNumWorkcenters] = useState()
+  const [wipPerWorkcenter, setWipPerWorkcenter] = useState()
+  const [numConstraints, setNumConstraints] = useState()
+  const [wipPerConstraint, setWipPerConstraint] = useState()
+
   return (
     <div className='App'>
       <div className='App-header'>Dice Game</div>
       <div className='Body'>
-        <UserInputs className='User-Inputs' />
-        <Factory className='Factory' />
+        <UserInputs
+          className='User-Inputs'
+          setRunNumber={setRunNumber}
+          setNumWorkdays={setNumWorkdays}
+          setNumWorkcenters={setNumWorkcenters}
+          setWipPerWorkcenter={setWipPerWorkcenter}
+          setNumConstraints={setNumConstraints}
+          setWipPerConstraint={setWipPerConstraint}
+        />
+        <Factory
+          className='Factory'
+          workcenters={workcenters}
+          runNumber={runNumber}
+          numWorkdays={numWorkdays}
+          numWorkcenters={numWorkcenters}
+          wipPerWorkcenter={wipPerWorkcenter}
+          numConstraints={numConstraints}
+          wipPerConstraint={wipPerConstraint}
+        />
       </div>
     </div>
   )
