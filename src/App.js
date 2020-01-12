8030e844e1efe600d7cdb87130db05fb7a553fd1
Doug Cole <idugcoal@gmail.com>
2020-01-12 14:03:13 -0800

    refactor separate settings into single object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import UserInputs from './components/user_inputs'
 import Factory from './components/factory'
@@ -6,31 +6,34 @@ import { getWorkcenters } from './components/fabricator'
 
 function App() {
   /** State */
-  const [runNumber, setRunNumber] = useState(4)
-  const [numWorkdays, setNumWorkdays] = useState(20)
-  const [numWorkcenters, setNumWorkcenters] = useState(6)
-  const [wipPerWorkcenter, setWipPerWorkcenter] = useState(4)
-  const [numConstraints, setNumConstraints] = useState(1)
-  const [wipPerConstraint, setWipPerConstraint] = useState(12)
+  const [workcenters, setWorkcenters] = useState()
+  const [settings, setSettings] = useState()
 
   /** Initial config */
-  const setup = { numWorkcenters, wipPerWorkcenter, wipPerConstraint }
-  const workcenters = getWorkcenters(setup)
+  useEffect(() => {
+    const _workcenters = !!settings ? getWorkcenters(settings) : []
+    setWorkcenters(_workcenters)
+  }, [settings])
+
+  /** Helper functions */
+  const startGame = () => {
+    console.log('settings before', settings)
+  }
+
   /** Template */
   return (
     <div className='App'>
       <div className='App-header'>Dice Game</div>
       <div className='Body'>
-        <UserInputs
-          className='User-Inputs'
-          setRunNumber={setRunNumber}
-          setNumWorkdays={setNumWorkdays}
-          setNumWorkcenters={setNumWorkcenters}
-          setWipPerWorkcenter={setWipPerWorkcenter}
-          setNumConstraints={setNumConstraints}
-          setWipPerConstraint={setWipPerConstraint}
-        />
-        <Factory className='Factory' workcenters={workcenters} />
+        <UserInputs className='User-Inputs' setSettings={setSettings} />
+        {workcenters && settings && (
+          <Factory
+            className='Factory'
+            workcenters={workcenters}
+            settings={settings}
+          />
+        )}
+        {settings && <div onClick={startGame}>Start game</div>}
       </div>
     </div>
   )
