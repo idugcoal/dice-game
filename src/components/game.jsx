006a8146aa8e8a04a487682f462591fc409128de
Doug Cole <idugcoal@gmail.com>
2020-01-12 17:25:23 -0800

    fix dice display logic

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import config from '../config'
 
 const Game = ({ step, setStep }) => {
   /** State */
-  // const [image, setImage] = useState()
+  const [image, setImage] = useState()
+
   /** Helper functions */
   const rollDie = () => {
     const max = 6
     const min = 1
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
+
   const onClick = () => {
-    return config.dice(rollDie())
     setStep(step + 1)
+    const diceImages = config.dice
+    const roll = rollDie()
+    setImage(diceImages[roll - 1])
   }
-  // const onClick = () => {}
-  console.log(rollDie())
-  const dice = config.dice
+
   /** Template */
   return (
     <div style={styles.container}>
       <div style={styles.title}>{`Game`}</div>
       <div style={styles.diceContainer} onClick={onClick}>
-        <img src={dice[rollDie()]} alt='die' />
+        {image && <img src={image} alt='die' />}
       </div>
     </div>
   )
