e16102c8cb2a161317a72a8abd11a14e3c445aa2
Doug Cole <idugcoal@gmail.com>
2020-01-18 12:24:37 -0800

    add rollDice function and comments

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import config from '../config'
+import React, {
+  // useState,
+  useEffect,
+} from 'react'
+// import config from '../config'
 
-const Game = ({ step, setStep }) => {
+const Game = ({ step, setStep, results, setResults }) => {
   /** State */
-  const [image, setImage] = useState()
 
   /** Helper functions */
   const rollDie = () => {
@@ -12,26 +14,44 @@ const Game = ({ step, setStep }) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  useEffect(() => {}, [step])
-  const onClick = () => {
+  const rollDice = count => {
+    let result = 0
+    for (let i = 0; i < count; i++) {
+      result += rollDie()
+    }
+    return result
+  }
+
+  useEffect(() => {
+    // when the step changes, that means the day has incremented
+    // for every new day:
+    //  for every workcenter:
+    //    roll dice
+    //      workcenter 1's dice roll determines the amount it completes (unless TOC)
+    //      every other workcenter completes the smaller of dice roll and wip
+    //    calculate each workcenter's output
+    //    route each workcenter's completed work
+    //      if TOC, constraint's wip also goes to workcenter 1's input
+    //  add the day's results object to the results array in state
+  })
+  const startGame = () => {
     setStep(step + 1)
-    const diceImages = config.dice
-    const roll = rollDie()
-    setImage(diceImages[roll - 1])
+    const roll = rollDice(2)
+    console.log('roll', roll)
   }
   /** Game */
-  // 1) roll die
+  // 1) roll dice
   // 2) set as inventory intake
   // 3) add inventory intake and WIP to create stock
-  // 4) roll die
-  //  a) if stock <= die, pass stock to next workcenter and remove from stock
-  //  b) if die < stock, pass die to next workcenter and remove from stock
+  // 4) roll dice
+  //  a) if stock <= dice, pass stock to next workcenter and remove from stock
+  //  b) if dice < stock, pass die to next workcenter and remove from stock
   /** Template */
   return (
     <div style={styles.container}>
-      <div style={styles.title}>{`Game`}</div>
-      <div style={styles.diceContainer} onClick={onClick}>
-        {image && <img src={image} alt='die' />}
+      {/* <div style={styles.title}>{`Game`}</div> */}
+      <div style={styles.startButton} onClick={startGame}>
+        {`Start`}
       </div>
     </div>
   )
@@ -42,13 +62,20 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
   },
-  title: {
-    textAlign: 'center',
-    fontSize: 24,
-  },
+  // title: {
+  //   textAlign: 'center',
+  //   fontSize: 24,
+  // },
   diceContainer: {
     width: 200,
     height: 200,
+  },
+  startButton: {
+    backgroundColor: '#4caf50',
+    fontSize: '24px',
+    margin: '16px',
+    color: 'white',
+    textAlign: 'center',
   },
 }
 export default Game
